35bf090c9af6843fada50b670d659761984e46f3
Magnus-Elias.Sletten <magnus-elias.sletten@student.uib.no>
2024-09-26 21:23:39 +0200

    Add user to docker container for experiments

--- .github/workflows/GetNewExperimentData.sh
+++ .github/workflows/GetNewExperimentData.sh
@@ -3,10 +3,8 @@
 GITHUB_USERNAME="MagnusSletten_Bot"  
 GITHUB_EMAIL="magnus.elias.sletten@gmail.com"  
 
-
 git config --global user.name "$GITHUB_USERNAME"
 git config --global user.email "$GITHUB_EMAIL"
-
 
 # Name of file we're going to store new filenames in:
 ORDERPARAMETER_FILE="orderparameters.txt"
@@ -25,17 +23,18 @@ cd $DATABANK_ABS_PATH
 # Find new added files in this branch relative to the other branch mentioned here:
 NEW_ORDERPARAMETER_FILES=$(git diff --name-only origin/$BRANCH_NAME origin/$TARGET_BRANCH -- Data/experiments/)
 
-if [-n $NEW_ORDERPARAMETER_FILES ]; then
+if [ -n "$NEW_ORDERPARAMETER_FILES" ]; then  # Added spaces around the square brackets
   echo "$NEW_ORDERPARAMETER_FILES" > "$ORDERPARAMETER_FILE"
   while IFS= read -r file; do
-    # Check if the file is a .dat file
     if [[ $file == *.dat ]]; then
       echo "Running data_to_json.py for $file"
       python3 "$DATABANK_ABS_PATH/data_to_json.py" -f "$DATABANK_ABS_PATH/$file"
       break   # Temporary for testing purposes.
-    else
+    fi  
+  done < "$ORDERPARAMETER_FILE"  
+else
   echo "No new files detected in $TARGET_DIR."
-fi
+fi  # Added missing 'fi' to close the outer if block
 
 rm "$ORDERPARAMETER_FILE"
 
