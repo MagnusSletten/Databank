e1909cd78de478b350ca36cd9c42c65ec2dbceb4
Magnus-Elias.Sletten <magnus-elias.sletten@student.uib.no>
2024-10-10 11:59:15 +0200

    Remove sorting for now

--- .github/workflows/RecomputeSimdata.sh
+++ .github/workflows/RecomputeSimdata.sh
@@ -29,7 +29,7 @@ WORK_DIR="/tmp/databank_workdir"
 mkdir -p "$WORK_DIR" || { echo "Failed to create work directory"; exit 1; }
 
 # Get sorted list of all .yaml files in BUILDDATABANKPATH
-sorted_files=($(find "$BUILDDATABANKPATH/info_files" -name "*.yaml" | sort -t '/' -k6n))
+sorted_files=($(find "$BUILDDATABANKPATH/info_files" -name "*.yaml"))
 
 
 # Calculate the subset of files based on $start_index and $end_index
