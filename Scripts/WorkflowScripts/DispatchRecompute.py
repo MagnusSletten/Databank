9f3cb150b43ad7ba72e507c369cbf6698f5e2416
Magnus-Elias.Sletten <magnus.elias.sletten@gmail.com>
2024-11-14 11:19:31 +0100

    Handle recompute all files

--- Scripts/WorkflowScripts/DispatchRecompute.py
+++ Scripts/WorkflowScripts/DispatchRecompute.py
@@ -4,21 +4,28 @@ import sys
 
 def main():
     # Read inputs from environment variables or arguments
-    number_of_runs = int(os.getenv('NUMBER_OF_RUNS'))
+    number_of_runners = int(os.getenv('number_of_runners'))
     start_index = int(os.getenv('START_INDEX'))
     end_index = int(os.getenv('END_INDEX'))
     working_branch_name = os.getenv('BRANCH_NAME')
+
+    # Check if end_index is -1, and set it to the last folder index based on the total count in info_files
+    if end_index == -1:
+        info_files_dir = 'Scripts/BuildDatabank/info_files'
+        folders = [name for name in os.listdir(info_files_dir) if os.path.isdir(os.path.join(info_files_dir, name))]
+        end_index = len(folders) - 1  # Set end_index based on the folder count
+    
     
     # Calculate total number of items
     total_items = end_index - start_index + 1
 
     # Calculate base chunk size and remainder
-    chunk_size = total_items // number_of_runs
-    remainder = total_items % number_of_runs
+    chunk_size = total_items // number_of_runners
+    remainder = total_items % number_of_runners
 
     current_start = start_index
 
-    for i in range(0, number_of_runs):
+    for i in range(0, number_of_runners):
         # Calculate end index for this chunk
         current_chunk_size = chunk_size
         if i == 0:
