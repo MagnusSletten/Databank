00b08d0014584cece0d31ba44f66a639dff3c64c
Magnus-Elias.Sletten <magnus.elias.sletten@gmail.com>
2024-11-23 16:17:46 +0100

    Sort files from initialize databank for consistency

--- Scripts/WorkflowScripts/PullPush.sh
+++ Scripts/WorkflowScripts/PullPush.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-git pull || {echo "git push failed";exit 1;}
+
+# Pull the latest changes
+git pull || { echo "git pull failed"; exit 1; }
+
+# Push changes to the repository
 git push https://x-access-token:$GITHUB_TOKEN@github.com/MagnusSletten/Databank.git || { echo "git push failed"; exit 1; }
