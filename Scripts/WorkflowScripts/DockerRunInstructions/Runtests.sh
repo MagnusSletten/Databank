5dba021854b82396b68aacdc8a6dcbcf6e678efd
Magnus-Elias.Sletten <magnus.elias.sletten@gmail.com>
2024-11-24 10:42:51 +0100

    Fix recompute scripts, make GetNewSimData.sh more effective
    The recompute now follows proper protocol of deleting relevant json files before running calcProperties.sh. This adds some convience scripts which will later be deleted like printSystemSize.py

--- Scripts/WorkflowScripts/DockerRunInstructions/Runtests.sh
+++ Scripts/WorkflowScripts/DockerRunInstructions/Runtests.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 git checkout $BRANCH_NAME
 git pull
-<<<<<<< HEAD
-cd /app/Databank/Scripts/tests/ &&
-source /usr/local/gromacs/bin/GMXRC &&
-pytest vs
-=======
 cd Scripts/tests/ &&
 source /usr/local/gromacs/bin/GMXRC &&
 pytest -vs
->>>>>>> dev_pipeline_compose
