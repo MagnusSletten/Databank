ff76e86b9ce614b29af3e14269a315b2705462b8
Magnus-Elias.Sletten <magnus.elias.sletten@gmail.com>
2024-11-25 17:27:12 +0100

    Sort databank based on ID instead of filepath

--- Scripts/WorkflowScripts/Workflow_utils.py
+++ Scripts/WorkflowScripts/Workflow_utils.py
@@ -5,8 +5,9 @@ Contains methods used for python scripts related to workflows.
 """
 
 
-#In order to get a consistent order of readme.yaml files we use sorting based on file path.
+#In order to get a consistent order of readme.yaml files we use sorting based on the ID.
 def sorted_databank():
     systems = list(initialize_databank())
-    systems.sort(key=lambda x: x["path"])
+    systems.sort(key=lambda x: x['ID'])
     return systems
+
