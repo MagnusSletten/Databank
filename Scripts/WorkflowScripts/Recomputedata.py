45b3b36bb7a9181aafa1b66b8d4365e9618b50cc
Magnus-Elias.Sletten <magnus.elias.sletten@gmail.com>
2024-11-26 13:23:08 +0100

    Change commit message of recompute folder to ID

--- Scripts/WorkflowScripts/Recomputedata.py
+++ Scripts/WorkflowScripts/Recomputedata.py
@@ -47,7 +47,7 @@ def run_calc_properties():
     except Exception as e:
         print(f"Unexpected error: {e}", flush=True)
         raise
-def git_commit_simulation_folder(folder_name, index):
+def git_commit_simulation_folder(system):
     """
     Pulls the latest changes, adds only JSON files from the specific simulation folder, and commits changes.
     """
@@ -56,11 +56,11 @@ def git_commit_simulation_folder(folder_name, index):
         print("Pulling latest changes before committing...", flush=True)
         subprocess.run(["git", "pull"], check=True)
         print("Successfully pulled latest changes.", flush=True)
-
+        folder_name = system["path"]
         json_files_dir = os.path.join("Data", "Simulations", folder_name)
         json_files_path = os.path.join("Data", "Simulations", folder_name, "*.json")
         matching_files = glob.glob(json_files_path)
-        
+        system_id = system["ID"]
         if not matching_files:
             print(f"No JSON files found in {json_files_path}. Skipping commit.", flush=True)
             return
@@ -69,7 +69,7 @@ def git_commit_simulation_folder(folder_name, index):
         #Add deletions of tracked .json files
         subprocess.run(["git", "add", "-u", json_files_dir], check=True)
         print(f"Staged JSON files: {matching_files}", flush=True)
-        commit_message = f"Processed simulation folder: {folder_name} at index: {index}"
+        commit_message = f"Processed simulation with ID: {system_id}"
         subprocess.run(["git", "commit", "-m", commit_message], check=True)
         print(f"Committed changes: {commit_message}", flush=True)
     except subprocess.CalledProcessError as e:
