3957bcd955c35896344de3b645fe7f9474666b9d
Magnus-Elias.Sletten <magnus.elias.sletten@gmail.com>
2024-11-29 14:44:14 +0100

    Use github context for repo name instead of hardcoding

--- Scripts/WorkflowScripts/Git_Push.sh
+++ Scripts/WorkflowScripts/Git_Push.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Push changes to the repository
-git push https://x-access-token:$GITHUB_TOKEN@github.com/$REPO_NAME.git || { echo "git push failed"; exit 1; }
+git push https://x-access-token:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.git || { echo "git push failed"; exit 1; }
